0368514f1e53f8c9d5f988e252fbbfb295658749
Ryan Ilagan <rilagan@gmail.com>
2013-12-29 07:19:25 -0600

    Initial Commit

---
+++ LanguageBridge/LanguageBridge.m
@@ -0,0 +1,13 @@
+//
+//  LanguageBridge.m
+//  LanguageBridge
+//
+//  Created by Ryan Ilagan on 12/29/13.
+//  Copyright (c) 2013 Ryan Ilagan. All rights reserved.
+//
+
+#import "LanguageBridge.h"
+
+@implementation LanguageBridge
+
+@end
---
+++ LanguageBridgeTests/LanguageBridgeTests.m
@@ -0,0 +1,34 @@
+//
+//  LanguageBridgeTests.m
+//  LanguageBridgeTests
+//
+//  Created by Ryan Ilagan on 12/29/13.
+//  Copyright (c) 2013 Ryan Ilagan. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface LanguageBridgeTests : XCTestCase
+
+@end
+
+@implementation LanguageBridgeTests
+
+- (void)setUp
+{
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown
+{
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample
+{
+    XCTFail(@"No implementation for \"%s\"", __PRETTY_FUNCTION__);
+}
+
+@end
